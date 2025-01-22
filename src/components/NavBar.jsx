55b6d54be9817bbfa32c6b90801ced346a707a23
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-22 21:58:10 +0000

    wrap  anchor tags  with Link tag

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,16 +2,17 @@ import { useState } from "react"
 import { MdLogin } from "react-icons/md"
 import { RxCross1, RxHamburgerMenu } from "react-icons/rx"
 import Image from "./Image"
+import { Link } from "react-router-dom"
 
 const NavBar = () => {
   const [open, setOpen] = useState(false)
   return (
     <div className="w-full h-16 md:h-20 flex items-center justify-between">
       {/* logo */}
-      <div className="flex items-center gap-4 text-2xl font-bold">
+      <Link to="/" className="flex items-center gap-4 text-2xl font-bold">
         <Image src="logo.png" alt="logo" w={42} h={42} />
         <span>Daily-Blog</span>
-      </div>
+      </Link>
       {/* mobile menu */}
       <div className="md:hidden">
         {/* mobile button */}
@@ -27,30 +28,30 @@ const NavBar = () => {
             open ? "-right-0" : "-right-[100%]"
           }`}
         >
-          <a href="/">Home</a>
-          <a href="/">Trending</a>
-          <a href="/">Most Popular</a>
-          <a href="/">About</a>
-          <a href="">
+          <Link to="/">Home</Link>
+          <Link to="/">Trending</Link>
+          <Link to="/">Most Popular</Link>
+          <Link to="/">About</Link>
+          <Link to="">
             <button className="py-2 px-4 rounded-3xl bg-green-500 text-white flex items-center gap-3">
               <MdLogin className="font-bold text-2xl" />
               Login
             </button>
-          </a>
+          </Link>
         </div>
       </div>
       {/* desktop menu */}
       <div className="hidden md:flex items-center gap-8 xl:gap-12 font-medium">
-        <a href="/">Home</a>
-        <a href="/">Trending</a>
-        <a href="/">Most Popular</a>
-        <a href="/">About</a>
-        <a href="">
+        <Link to="/">Home</Link>
+        <Link to="/">Trending</Link>
+        <Link to="/">Most Popular</Link>
+        <Link to="/">About</Link>
+        <Link to="">
           <button className="py-2 px-4 rounded-3xl bg-green-500 text-white flex items-center gap-3">
             <MdLogin className="font-bold text-2xl" />
             Login
           </button>
-        </a>
+        </Link>
       </div>
     </div>
   )

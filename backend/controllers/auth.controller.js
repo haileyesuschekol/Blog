bbca017995133a1cb353144f3fa897be9843ff83
Haileyesus Chekol <teamdevs7@gmail.com>
2025-01-29 23:16:03 +0000

    setup auth route nd install dependencies

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,7 @@
+export const signup = (req, res) => {}
+export const login = (req, res) => {}
+export const verifyEmail = (req, res) => {}
+export const forgotPassword = (req, res) => {}
+export const resetPassword = (req, res) => {}
+export const logout = (req, res) => {}
+export const checkAuth = (req, res) => {}
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import connectToDb from "./lib/connectDb.js"
 import userRouter from "./routes/user.route.js"
 import postRouter from "./routes/post.route.js"
 import commentRouter from "./routes/comment.route.js"
+import authRouter from "./routes/auth.route.js"
 
 const app = express()
 app.use(express.json())
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use("/api/users", userRouter)
 app.use("/api/posts", postRouter)
 app.use("/api/comments", commentRouter)
+app.use("/api/auth", authRouter)
 
 //errror handler
 app.use((error, req, res, next) => {

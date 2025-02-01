bcc919684f505ee99852b1f282e6e13f66a55922
Haileyesus Chekol <teamdevs7@gmail.com>
2025-02-01 15:35:20 +0000

    add saved post schema in user model

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -11,6 +11,10 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
+    savedPost: {
+      type: [String],
+      default: [],
+    },
     password: {
       type: String,
       required: true,

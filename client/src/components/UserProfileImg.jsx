c5cb37e0af763e7afe53336386905965d54c73f9
Haileyesus Chekol <teamdevs7@gmail.com>
2025-02-03 21:12:32 +0000

    add icons to drop down and event listners

--- client/src/components/UserProfileImg.jsx
+++ client/src/components/UserProfileImg.jsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react"
+import { useEffect, useMemo, useRef, useState } from "react"
 import { Link } from "react-router-dom"
 import { CgProfile } from "react-icons/cg"
 import { IoSettingsOutline } from "react-icons/io5"
@@ -14,18 +14,27 @@ const getRandomColor = () => {
 }
 
 export default function UserProfileImg() {
+  const dropdownRef = useRef(null)
+  useEffect(() => {
+    function handleClickOutside(event) {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+    document.addEventListener("mousedown", handleClickOutside)
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside)
+    }
+  }, [])
+
   const [isOpen, setIsOpen] = useState(false)
   const bgColor = useMemo(() => getRandomColor(), [])
 
   return (
-    // <Link
-    //   className=" rounded-full w-12 h-12  flex flex-row justify-center items-center"
-    //   style={{ backgroundColor: bgColor }}
-    // >
-    //   <h2 className="text-lg font-bold  text-white ">N</h2>
-    // </Link>
-
-    <div className="flex flex-col relative items-center justify-center h-screen z-10">
+    <div
+      className="flex flex-col relative items-center justify-center h-screen z-10"
+      ref={dropdownRef}
+    >
       <button
         onClick={() => setIsOpen(!isOpen)}
         className=" rounded-full border-2 border-green-400 w-12 h-12  flex flex-row justify-center items-center"
@@ -40,17 +49,17 @@ export default function UserProfileImg() {
         className="overflow-hidden w-32 bg-white rounded-lg shadow-lg"
       >
         <div
-          className="p-6 mt-1 text-center absolute left-3 bg-white   rounded-lg flex flex-col justify-center items-center gap-2"
+          className="p-6 mt-1 text-center absolute left-3 bg-white rounded-lg flex flex-col justify-center items-start gap-2"
           // style={{ backgroundColor: bgColor }}
         >
-          <p className=" text-gray-800">
+          <p className=" text-gray-800 flex flex-row gap-3 items-center">
             <CgProfile /> Profile
           </p>
-          <p className="text-gray-800">
+          <p className="text-gray-800 flex flex-row gap-3 items-center">
             <IoSettingsOutline />
             Setting
           </p>
-          <p className="text-gray-800">
+          <p className="text-gray-800 flex flex-row gap-3 items-center">
             <MdLogout />
             Logout
           </p>

e46c3b6670780fb7e758823751cf699a653e0455
Haileyesus Chekol <teamdevs7@gmail.com>
2025-02-08 10:48:54 +0000

    render posts

--- client/src/components/PostList.jsx
+++ client/src/components/PostList.jsx
@@ -6,12 +6,9 @@ const PostList = () => {
   console.log(postData)
   return (
     <div className="flex flex-col gap-12 mb-8">
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
-      <PostListItem />
+      {postData.map((post) => (
+        <PostListItem key={post._id} post={post} />
+      ))}
     </div>
   )
 }

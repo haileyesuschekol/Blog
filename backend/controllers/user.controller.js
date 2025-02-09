42e6acdcaed5fe47f26e4cf3327fbbb831ebf634
Haileyesus Chekol <teamdevs7@gmail.com>
2025-02-09 21:39:11 +0000

    create savepost route

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -13,4 +13,25 @@ export const getUserSavedPost = async (req, res) => {
   res.status(200).json(user.savedPost)
 }
 
-export const savePost = async (req, res) => {}
+export const savePost = async (req, res) => {
+  const userId = req.userId
+  const { postId } = req.body
+
+  if (!userId) {
+    return res
+      .status(403)
+      .json({ success: false, message: "Not authenticated!" })
+  }
+
+  const user = await User.findOne({ userId })
+
+  const isSaved = user.savedPost.some((p) => p === postId)
+
+  if (!isSaved) {
+    await User.findByIdAndUpdate(user._id, { $push: { savedPost: postId } })
+  } else {
+    await User.findByIdAndUpdate(user._id, { $pull: { savedPost: postId } })
+  }
+
+  res.status(200).json(isSaved ? "Post unsaved" : "Post saved")
+}

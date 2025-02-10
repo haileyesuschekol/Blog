937a9472972ceca625e92489a54f9df03f7e97b0
Haileyesus Chekol <teamdevs7@gmail.com>
2025-02-10 00:45:57 +0000

    add user auth middleware to user route

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express"
 
 import { savePost, getUserSavedPost } from "../controllers/user.controller.js"
+import { isUserAuth } from "../middleware/auth.js"
 
 const router = express.Router()
 
-router.get("/saved", getUserSavedPost)
-router.patch("/saved", savePost)
+router.get("/saved", isUserAuth, getUserSavedPost)
+router.patch("/saved", isUserAuth, savePost)
 
 export default router

e4a4300d8a4e102b304670102b9b3795da935a8a
Haileyesus Chekol <teamdevs7@gmail.com>
2025-02-23 19:59:32 +0000

    add content niche

--- client/src/routes/PostListPage.jsx
+++ client/src/routes/PostListPage.jsx
@@ -1,13 +1,17 @@
 import { useState } from "react"
 import PostList from "../components/PostList"
 import SideMenu from "../components/SideMenu"
+import { useSearchParams } from "react-router-dom"
 
 const PostListPage = () => {
   const [open, setOpen] = useState(false)
+  const [searchParams] = useSearchParams()
+
+  const category = searchParams.get("cat")
 
   return (
     <div className="">
-      <h1 className="mb-8 text-2xl">Development Blog</h1>
+      <h1 className="mb-8 text-2xl">{category}</h1>
       <button
         onClick={() => setOpen((prev) => !prev)}
         className="bg-green-500 text-sm text-white px-4 py-2 rounded-xl mb-4 md:hidden"
